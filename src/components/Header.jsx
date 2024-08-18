218ff0c558ace572d4d9a97d606ba10f3570f648
lvcas-ca <lucas_.caceres@hotmail.com.ar>
2024-08-18 11:50:16 -0300

    test brach

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { NavBar } from "./NavBar"
 
 export const Header = () =>{
     return(
-        <div className="flex flex-row justify-between gap-10 p-[15px]">
+        <div className="flex flex-row justify-between gap-10 p-[15px] bg-orange-600">
             <img className="" src={siteLogo} alt="Logo" width={100} height={80} />
             <NavBar/>
             <button>Contacto</button>
